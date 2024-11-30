621ff4b7cdfafe5b7c136f36dbbff0fcf47a913e
aman85662 <118710449+aman85662@users.noreply.github.com>
2024-11-30 14:38:25 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function opensidebar(){
+    const sidebarElement = document.getElementById('sidebar');
+    sidebarElement.style.visibility = 'visible';
+}
+function closesidebar(){
+    const sidebarElement = document.getElementById('sidebar');
+    sidebarElement.style.visibility = 'hidden';
+}
